c183ea1d7aa3d70e5abfe7808b9c15cb648eada6
Lewuathe <lewuathe@me.com>
2013-11-27 22:26:58 +0900

    substring titles

--- src/main/java/com/lewuathe/plugins/hckrnews/HckrnewsWidget.java
+++ src/main/java/com/lewuathe/plugins/hckrnews/HckrnewsWidget.java
@@ -16,6 +16,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,6 +38,11 @@ public class HckrnewsWidget extends Widget {
 
 	public void setNewslist(List<News> newsList) {
 		this.newsList = newsList;
+	}
+
+	public String getLastUpdatedStr() {
+		System.out.println("------------------- getLastUpdatedStr ----------------------");
+		return lastUpdated.toString();
 	}
 
     public Date getLastUpdated() {
@@ -71,8 +77,28 @@ public class HckrnewsWidget extends Widget {
 			this.postedBy = "Nobody";
 		}
 
+		public String cut(String s, int n) {
+			byte[] utf8 = s.getBytes();
+			if (utf8.length < n) { 
+				n = utf8.length;
+				return s;
+			}
+			int n16 = 0;
+			boolean extraLong = false;
+			int i = 0;
+			while (i < n) {
+				n16 += (extraLong) ? 2 : 1;
+				extraLong = false;
+				if ((utf8[i] & 0x80) == 0) i += 1;
+				else if ((utf8[i] & 0xC0) == 0x80) i += 2;
+				else if ((utf8[i] & 0xE0) == 0xC0) i += 3;
+				else { i += 4; extraLong = true; }
+			}
+			return s.substring(0,n16) + "...";
+		}
+
 		public void setTitle(String title) {
-			this.title = title;
+			this.title = cut(title, 30);
 		}
 
 		public void setUrl(String url) {
@@ -109,16 +135,17 @@ public class HckrnewsWidget extends Widget {
 
 		@Override
 		protected void doRun() throws Exception {
+			Date lastUpdated = null;;
 			List<News> newsList = new ArrayList<News>();
 			String json = loadNews();
 
-			System.out.println(json);
 			JsonFactory factory = new JsonFactory();
 			JsonParser parser = factory.createJsonParser(json);
 
 			while (parser.nextToken() != JsonToken.END_OBJECT) {
 				String name = parser.getCurrentName();
 				if (name != null) {
+					lastUpdated = new Date();
 					if (name.equals("items")) {
 						while (parser.nextToken() != JsonToken.END_ARRAY) {
 							name = parser.getCurrentName();
@@ -156,6 +183,7 @@ public class HckrnewsWidget extends Widget {
 			for (Widget w : Hudson.getInstance().getWidgets()) {
 				if (w instanceof HckrnewsWidget) {
 					HckrnewsWidget hw = (HckrnewsWidget) w;
+					hw.setLastUpdated(lastUpdated);
 					hw.setNewslist(newsList);
 				}
             }
@@ -163,6 +191,25 @@ public class HckrnewsWidget extends Widget {
 			
         protected String loadNews() {
 			InputStream is = null;
+			URL url = null;
+			try {
+				url = new URL(API_URL);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+			for (int i = 0; i < 5; i++) {
+				System.out.println("Retry: " + i);
+				try {
+					is = url.openStream();
+					return IOUtils.toString(is, "UTF-8");
+				} catch (IOException e) {
+					continue;
+				}
+			}
+			System.out.println("Get failed");
+			throw new RuntimeException();
+			/*
             try {
                 is = new URL(API_URL).openStream();
                 return IOUtils.toString(is, "Shift_JIS");
@@ -173,6 +220,7 @@ public class HckrnewsWidget extends Widget {
             } finally {
                 IOUtils.closeQuietly(is);
             }
+			*/
         }
 
 	}
